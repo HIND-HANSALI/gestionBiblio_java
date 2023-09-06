f6ea85c4a35e3009a9492cd1f0157e1f3aa344a0
HIND-HANSALI <hindhsl111@gmail.com>
2023-09-06 21:04:12 +0100

    adding classes

--- src/main/java/org/example/Auteur.java
+++ src/main/java/org/example/Auteur.java
@@ -3,6 +3,26 @@ package org.example;
 public class Auteur {
     public int id;
     public String name;
+    public Auteur(int id, String name) {
+        this.id = id;
+        this.name = name;
+
+    }
+    public Auteur(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     //getters & setters
     public int getId() {
--- src/main/java/org/example/Bibliothecaire.java
+++ src/main/java/org/example/Bibliothecaire.java
@@ -3,6 +3,26 @@ package org.example;
 public class Bibliothecaire {
     public int id;
     public String name;
+    public String email;
+
+    public Bibliothecaire(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    public Bibliothecaire(int id, String name) {
+        this.id = id;
+        this.name = name;
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     //getters & setters
     public int getId() {
