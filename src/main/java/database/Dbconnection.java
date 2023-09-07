27931a43da49ea4d287e597620dd01bc944df4e2
HIND-HANSALI <hindhsl111@gmail.com>
2023-09-07 20:07:24 +0100

    fix an error in connection

--- src/main/java/database/Dbconnection.java
+++ src/main/java/database/Dbconnection.java
@@ -6,19 +6,17 @@ import java.sql.SQLException;
 
 public class Dbconnection {
 
-
-    private static Connection connection;
     public static Connection getConnection() {
+        Connection connection = null;
 
-        if (connection == null) {
-            try {
-                // Create a connection to the database
-                connection = DriverManager.getConnection("jdbc:mysql://localhost:3308/gestion_biblio_minisas", "root", "");
-                System.out.println("Connected to the database");
-            } catch (SQLException e) {
-                System.err.println("Error connecting to the database: " + e.getMessage());
-            }
+        try {
+            // Create a connection to the database
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3308/gestion_biblio_minisas", "root", "");
+            System.out.println("Connected to the database");
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database: " + e.getMessage());
         }
+
         return connection;
     }
 }
